919bc5303a3caad1eb898490b2347da6aeb2b629
Igor Couto <igor.fcouto@gmail.com>
2016-05-30 20:41:25 -0300

    Refatorando apenas comentários e testando commit

--- src/java/Model/Usuario.java
+++ src/java/Model/Usuario.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 /**
- *
- * @author Thallys pc
+ * @author Igor, Mauricio, Victor
  */
 public class Usuario {
     
@@ -40,8 +34,5 @@ public class Usuario {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
-    }
-    
-    
-    
+    }  
 }
