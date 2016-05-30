4414854120819d12ce43e33ea2499fca89b79dd8
Thallys pc <Thallys pc@Thallys>
2016-05-30 20:31:40 -0300

    Modicaicao Carrinho. Criação do FrontController

--- src/java/Model/Carrinho.java
+++ src/java/Model/Carrinho.java
@@ -5,10 +5,25 @@
  */
 package Model;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author Thallys pc
  */
 public class Carrinho {
+    
+    private LinkedList<Produto> produtos;
+
+    public LinkedList<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(LinkedList<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public Carrinho() {
+    }
     
 }
