7d3b42b2baeffe96b8c0f6e8cc030326ae5d7522
Igor Couto <igor.fcouto@gmail.com>
2016-05-31 12:36:04 -0300

    Criada classe Action ListaPordutos que servira para consultar o banco e exibir os produtos

---
+++ src/java/Action/ListaProdutos.java
@@ -0,0 +1,15 @@
+package Action;
+
+import controller.Action;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListaProdutos implements Action{
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+    }
+    
+}
